3ca67d740a85a7d65309eee93daa023292933f17
Elmay05 <150318195+Elmay05@users.noreply.github.com>
2023-11-15 23:17:17 -0600

    Corregido Cuenta_bancaria.cpp

--- Cuenta_bancaria.cpp
+++ Cuenta_bancaria.cpp
@@ -5,8 +5,19 @@
 //A01712114-Omar Emilio Casillas Alday
 int main(){
     //pruebas primera clase
+    std::string nom;
+    int ing;
+    float inter;
+
+
     CuentaBanco cuenta_2;
-    CuentaBanco cuenta_1("Juan", 1000, 11.7);
+    std::cout<<"Inserte nombre:";
+    std::cin>>nom;
+    std::cout<<"Inserte ingreso inicial:";
+    std::cin>>ing;
+    std::cout<<"Inserte intereses:";
+    std::cin>>inter;
+    CuentaBanco cuenta_1(nom, ing, inter);
     std::cout<<"Nombre:"<<cuenta_1.get_nombre()<<std::endl;
     std::cout<<"Ingreso inicial:"<<cuenta_1.get_ingreso_inicial()<<std::endl;
     std::cout<<"Intereses:"<<cuenta_1.get_interes()<<"%"<<std::endl;
@@ -17,9 +28,23 @@ int main(){
     std::cout<<"Prueva cuenta_2:"<<std::endl;
     cuenta_2.resumen();
     std::cout<<'\n';
+
+
+
+
+
     //pruebas segunda clase
+    int dep;
+    std::cout<<"Inserte nombre:";
+    std::cin>>nom;
+    std::cout<<"Inserte ingreso inicial:";
+    std::cin>>ing;
+    std::cout<<"Inserte deposito mensual:";
+    std::cin>>dep;
+
     IngresosMensuales cuenta_3;
-    IngresosMensuales cuenta_4("Sofia", 2000, 1000);
+
+    IngresosMensuales cuenta_4(nom, ing, dep);
     std::cout<<"Nombre:"<<cuenta_4.get_nombre()<<std::endl;
     std::cout<<"Ingreso inicial:"<<cuenta_4.get_ingreso_inicial()<<std::endl;
     std::cout<<"Deposito mensual: $"<<cuenta_4.get_depositoM()<<std::endl;
@@ -31,20 +56,30 @@ int main(){
     cuenta_3.resumen();
     std::cout<<'\n';
     //pruebas tercera clase
+    int ano;
+    std::cout<<"Inserte nombre:";
+    std::cin>>nom;
+    std::cout<<"Inserte ingreso inicial:";
+    std::cin>>ing;
+    std::cout<<"Inserte deposito mensual:";
+    std::cin>>dep;
+    std::cout<<"Inserte intereses:";
+    std::cin>>inter;
+    std::cout<<"Inserte años:";
+    std::cin>>ano;
     GananciasAnual cuenta_5;
-    GananciasAnual cuenta_6("Sofia", 1000, 11.7,500, 1);
+    GananciasAnual cuenta_6(nom, ing, inter,dep, ano);
     std::cout<<"Nombre:"<<cuenta_6.get_nombre()<<std::endl;
     std::cout<<"Ingreso inicial:"<<cuenta_6.get_ingreso_inicial()<<std::endl;
     std::cout<<"Intereses:"<<cuenta_6.get_intereses()<<std::endl;
     std::cout<<"Deposito mensual: $"<<cuenta_6.get_depositoM()<<std::endl;
-    std::cout<<"A�os:"<<cuenta_6.get_anos()<<std::endl;
-    cuenta_6.set_nombre("Ximena");
+    std::cout<<"Años:"<<cuenta_6.get_anos()<<std::endl;
+    cuenta_6.ganancias();
+    cuenta_6.set_nombre("Emilio");
     cuenta_6.set_ingreso_inicial(1000);
     cuenta_6.set_intereses(11.7);
     cuenta_6.set_depositoM(500);
-    cuenta_6.set_anos(3);
+    cuenta_6.set_anos(2);
     cuenta_6.ganancias();
     std::cout<<"Prueva cuenta_5:"<<std::endl;
     cuenta_5.ganancias();
-
-}
