bdebf7bb1522e5954e95ed43f931e2defdf8b3b8
Elmay05 <150318195+Elmay05@users.noreply.github.com>
2023-11-22 23:42:27 -0600

    Cuenta_bancaria.cpp

--- Cuenta_bancaria.cpp
+++ Cuenta_bancaria.cpp
@@ -24,9 +24,9 @@ int main(){
     cuenta_1.set_nombre("Roberto");
     cuenta_1.set_ingreso_inicial(2000);
     cuenta_1.set_intereses(7.4);
-    cuenta_1.resumen();
+    cuenta_1.resumencuenta();
     std::cout<<"Prueva cuenta_2:"<<std::endl;
-    cuenta_2.resumen();
+    cuenta_2.resumencuenta();
     std::cout<<'\n';
 
 
@@ -39,12 +39,14 @@ int main(){
     std::cin>>nom;
     std::cout<<"Inserte ingreso inicial:";
     std::cin>>ing;
+    std::cout<<"Inserte intereses:";
+    std::cin>>inter;
     std::cout<<"Inserte deposito mensual:";
     std::cin>>dep;
 
     IngresosMensuales cuenta_3;
 
-    IngresosMensuales cuenta_4(nom, ing, dep);
+    IngresosMensuales cuenta_4(nom, ing, inter, dep);
     std::cout<<"Nombre:"<<cuenta_4.get_nombre()<<std::endl;
     std::cout<<"Ingreso inicial:"<<cuenta_4.get_ingreso_inicial()<<std::endl;
     std::cout<<"Deposito mensual: $"<<cuenta_4.get_depositoM()<<std::endl;
@@ -52,9 +54,10 @@ int main(){
     cuenta_4.set_ingreso_inicial(5000);
     cuenta_4.set_depositoM(500);
     cuenta_4.resumen();
-    std::cout<<"Prueva cuenta_3:"<<std::endl;
+    std::cout<<"Prueba cuenta_3:"<<std::endl;
     cuenta_3.resumen();
     std::cout<<'\n';
+
     //pruebas tercera clase
     int ano;
     std::cout<<"Inserte nombre:";
@@ -71,7 +74,7 @@ int main(){
     GananciasAnual cuenta_6(nom, ing, inter,dep, ano);
     std::cout<<"Nombre:"<<cuenta_6.get_nombre()<<std::endl;
     std::cout<<"Ingreso inicial:"<<cuenta_6.get_ingreso_inicial()<<std::endl;
-    std::cout<<"Intereses:"<<cuenta_6.get_intereses()<<std::endl;
+    std::cout<<"Intereses:"<<cuenta_6.get_interes()<<std::endl;
     std::cout<<"Deposito mensual: $"<<cuenta_6.get_depositoM()<<std::endl;
     std::cout<<"Años:"<<cuenta_6.get_anos()<<std::endl;
     cuenta_6.ganancias();
@@ -81,5 +84,7 @@ int main(){
     cuenta_6.set_depositoM(500);
     cuenta_6.set_anos(2);
     cuenta_6.ganancias();
-    std::cout<<"Prueva cuenta_5:"<<std::endl;
+    std::cout<<"Prueba cuenta_5:"<<std::endl;
     cuenta_5.ganancias();
+
+}
