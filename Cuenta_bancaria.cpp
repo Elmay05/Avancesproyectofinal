979d696f34f212a44e50188992adba60b45b51a5
Elmay05 <150318195+Elmay05@users.noreply.github.com>
2023-11-28 22:55:10 -0600

    Cuenta_bancaria.cpp

--- Cuenta_bancaria.cpp
+++ Cuenta_bancaria.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
 #include "Banco.h"
-
-//A01712114-Omar Emilio Casillas Alday
+/*
+ *Main de proyecto plan de finanzas 
+ *Omar Emilio Casillas Alday 
+ *A01712114
+ *30/11/2023
+ *
+ */
 int main(){
 
-
+    //Variables para los valores a solicitar
     std::string nom;
     int ing;
     float inter;
     int dep;
     int yea;
-
+    //Peidmos los valores para crear el objeto
     std::cout<<"Inserte nombre:";
     std::cin>>nom;
     std::cout<<"Inserte ingreso inicial:";
@@ -22,10 +27,11 @@ int main(){
     std::cin>>dep;
     std::cout<<"Inserte por cuantos años:";
     std::cin>>yea;
+    //Asignamos los valores al objeto
     GananciasAnual cuenta_1(nom, ing, inter, dep,yea);
-
+    //Mostramos ganancias y resumen de cuenta
     cuenta_1.ganancias();
-    //Prueba constructor vacio
+    //Prueba con constructor vacio
     GananciasAnual cuenta_2;
     cuenta_2.resumen();
     cuenta_2.ganancias();
